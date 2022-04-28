08b9cc04a7241bd090e70722ffbf8d2c2ed01797
BharSat <69454005+BharSat@users.noreply.github.com>
2022-04-28 12:57:55 +0530

    correct package name

--- src/quick/game/app/SimpleGame.java
+++ src/quick/game/app/SimpleGame.java
@@ -1,22 +1,12 @@
-package ;
+package quick.game.app;
 
 import com.jme3.app.SimpleApplication;
 import quick.game.core.SimpleSceneState;
-import com.jme3.math.Vector3f;
-import com.jme3.bullet.BulletAppState;
 
 public class SimpleGame extends SimpleApplication {
 
-    public static void main(String[] args){
-        SimpleGame app = new SimpleGame();
-        app.start();
-    }
+    public static void main(String[] args){}
 
     @Override
-    public void simpleInitApp() {
-        stateManager.attach(BulletAppState);
-        stateManager.attach(SimpleSceneState);
-        stateManager.getState(SimpleSceneState.class).createFloor(new Vector3f(-10f, 0f, -10f), new Vector3f(10f, 0f, 10f));
-        // continue...
-    }
+    public void simpleInitApp() {}
 }
