0ce8ec139743fc22d5efba680f30d19f2ea91d38
BharSat <69454005+BharSat@users.noreply.github.com>
2022-04-28 12:29:17 +0530

    Added support for xml parsing. not complete.

---
+++ src/quick/game/core/scene/XmlReaderState.java
@@ -0,0 +1,85 @@
+package quick.game.core.scene;
+
+import com.jme3.app.Application;
+import com.jme3.app.SimpleApplication;
+import com.jme3.app.state.AppStateManager;
+import com.jme3.app.state.BaseAppState;
+import com.jme3.asset.AssetManager;
+import com.jme3.bullet.BulletAppState;
+import com.jme3.input.InputManager;
+import com.jme3.renderer.ViewPort;
+import com.jme3.scene.Node;
+
+import com.jme3.math.Vector3f;
+
+public class XmlReaderState extends BaseAppState {
+    /*AppState to aid quick.game.file.xml.XmlParser in xml parsing.
+    Automatically attaches SimpleSceneState if not
+    already attached.
+    Main XmlParser in quick.game.file.xml.XmlParser .*/
+    private SimpleApplication app;
+    private Node              rootNode;
+    private AssetManager      assetManager;
+    private AppStateManager   stateManager;
+    private InputManager      inputManager;
+    private ViewPort          viewPort;
+    private BulletAppState    physics;
+    
+    @Override
+    protected void initialize(Application appB) {
+        this.app = (SimpleApplication) app;
+        this.rootNode     = this.app.getRootNode();
+        this.assetManager = this.app.getAssetManager();
+        this.stateManager = this.app.getStateManager();
+        this.inputManager = this.app.getInputManager();
+        this.viewPort     = this.app.getViewPort();
+        this.physics      = this.stateManager.getState(BulletAppState.class);
+        if (not stateManager.hasState(SimpleSceneState.class)) {
+            stateManager.attach(SimpleSceneState);
+        }
+    }
+
+    @Override
+    protected void cleanup(Application app) {}
+
+    @Override
+    protected void onEnable() {}
+
+    @Override
+    protected void onDisable() {}
+
+    @Override
+    public void update(float tpf) {}
+
+    void floor(String coords){
+        String[] sepCoords = coords.split(",");
+        Vector3f start = new Vector3f(Integer.parseInt(sepCoords[0]), 0, Integer.parseInt(sepCoords[1]));
+        Vector3f end = new Vector3f(Integer.parseInt(sepCoords[2]), 0, Integer.parseInt(sepCoords[3]));
+        SimpleSceneState sceneBuilderState = (SimpleSceneState)stateManager.getState(SimpleSceneState.class);
+        sceneBuilderState.createFloor(start, end);
+    }
+
+    void wall(String coords){
+        String[] sepCoords = coords.split(",");
+        Vector3f start = new Vector3f(Integer.parseInt(sepCoords[0]), 0, Integer.parseInt(sepCoords[1]));
+        Vector3f end = new Vector3f(Integer.parseInt(sepCoords[2]), 0, Integer.parseInt(sepCoords[3]));
+        SimpleSceneState sceneBuilderState = (SimpleSceneState)stateManager.getState(SimpleSceneState.class);
+        sceneBuilderState.createWall(start, end);
+    }
+
+    void light(String coords){
+        String[] sepCoords = coords.split(",");
+        Vector3f direction = new Vector3f(Integer.parseInt(sepCoords[0]), Integer.parseInt(sepCoords[1]), Integer.parseInt(sepCoords[2]));
+        SimpleSceneState sceneBuilderState = (SimpleSceneState)stateManager.getState(SimpleSceneState.class);
+        sceneBuilderState.startLight(direction);
+    }
+
+    void region(String coords){
+        String[] sepCoords = coords.split(",");
+        Vector3f start = new Vector3f(Integer.parseInt(sepCoords[0]), 0, Integer.parseInt(sepCoords[1]));
+        Vector3f end = new Vector3f(Integer.parseInt(sepCoords[2]), 0, Integer.parseInt(sepCoords[3]));
+        SimpleSceneState sceneBuilderState = (SimpleSceneState)stateManager.getState(SimpleSceneState.class);
+        sceneBuilderState.createRegion(start, end);
+    }
+
+}
---
+++ src/quick/game/file/xml/XmlParser.java
@@ -0,0 +1,44 @@
+package quick.games.file.xml;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+import quick.game.core.scene.XmlReaderState;
+
+public class XmlParser extends DefaultHandler {
+
+   boolean floor = false;
+   boolean wall = false;
+   boolean light = false;
+   boolean region = false;
+   XmlReaderState reader;
+
+   public void init(@notNull XmlReaderState xml){
+       this.reader = xml;
+   }
+
+   @Override
+   public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+       if (qName.equals("floor")) {
+           reader.floor(attributes.getValue("coords"));
+       }
+       else if (qName.equals("wall")) {
+           reader.wall(attributes.getValue("coords"));
+       }
+       else if (qName=="light") {
+           reader.light(reader.floor(attributes.getValue("coords")););
+       }
+       else if (qName=="region") {
+           reader.region(reader.floor(attributes.getValue("coords")););
+       }
+   }
+
+   @Override
+   public void endElement(String uri, String localName, String qName) throws SAXException {
+      
+   }
+
+   @Override
+   public void characters(char ch[], int start, int length) throws SAXException {}
+}
