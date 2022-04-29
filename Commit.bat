54da517239e6a696ee995fc43dd1ea14cee14524
bhargav <bhargavmbalaji@gmail.com>
2022-04-29 15:32:17 +0530

    bat file to upload files from computer to repo after editing

---
+++ Commit.bat
@@ -0,0 +1,5 @@
+Copy "E:\Programs\JMonkeyEngine\jmonkeyplatform\Projects\game\src" "E:\Programs\JMonkeyEngine\jmonkeyplatform\Projects\game\github\src"
+git init
+git add .
+git commit
+git push https://github.com/BharSat/QuickGames-Jmonkey.git main
