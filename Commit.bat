7b0673d3c9ab8d10917b7f5f31b34d2f0b396d6b
BharSat <69454005+BharSat@users.noreply.github.com>
2022-04-30 11:44:08 +0530

    Delete Commit.bat

--- Commit.bat
+++
@@ -1,6 +0,0 @@
-Xcopy "E:\Programs\JMonkeyEngine\jmonkeyplatform\Projects\game\src" "E:\Programs\JMonkeyEngine\jmonkeyplatform\Projects\game\github\src" /E /H /C /I /Y
-git pull https://github.com/BharSat/QuickGames-Jmonkey.git main
-git init
-git add .
-git commit
-git push https://github.com/BharSat/QuickGames-Jmonkey.git main
